666312cfc0b6e1f8cad9e766a939a042025dfff5
David Whitlock <alovedalongthe@gmail.com>
2014-12-05 16:14:24 +0700

    Updated config

--- setup_website.sh
+++ setup_website.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install -y apache2 libapache2-mod-wsgi
 sudo a2enmod wsgi
 sudo apt-get install -y python3-dev python3-pip python3-software-properties software-properties-common \
-    postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3
+    postgresql-9.3 postgresql-server-dev-9.3
 sudo /etc/init.d/postgresql start &&\
     sudo -u postgres psql -c "CREATE USER comein WITH PASSWORD 'comein' CREATEDB;" &&\
     sudo -u postgres psql -c "CREATE DATABASE comein WITH OWNER comein"
--- uni_web.wsgi
+++ uni_web.wsgi
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
+import os
 import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, '/home/$USER/uni_web/')
+sys.path.insert(0, os.path.join(os.path.expanduser('~'), 'uni_web'))
 
 from website import app as application
 application.secret_key = b'\xda\x9a\xfa6\x0b\xf5`\xf8GS\xbb\xca\x9c\xb2\xed\x87\xdc\xf4 \xd6eY\xcf\x82'
