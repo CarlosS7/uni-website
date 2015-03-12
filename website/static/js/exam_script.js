165ba0352168dceb909e0b2656b7df33399a0712
David Whitlock <alovedalongthe@gmail.com>
2015-03-12 18:13:24 +0700

    Reorganized javascript code

--- website/static/js/exam_script.js
+++ website/static/js/exam_script.js
@@ -1,38 +1,41 @@
-function postJSON(url, token, data) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('X-CSRF-Token', token);
-    xhr.send(JSON.stringify(data));
-}
-
-function startExam() {
-    var audio = document.getElementById('listening');
-
-    document.getElementById('exam-body').style.display = 'block';
-    document.querySelector('.slide').className = 'slide-up';
-    if (audio) {
-        audio.play();
-    }
-    setInterval(function() {
-        updateResults();
-    }, 1000 * 60 * 5);
-}
-
-function updateResults() {
-    var form = document.forms[0];
-    var data = {};
-
-    for (var i = 0, ii = form.length; i < ii; i++) {
-        var input = form[i];
-        if (input.type === 'radio' && input.checked) {
-            data[input.name] = input.value;
+var Exam = (function () {
+    var postJSON = function (url, token, data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.setRequestHeader('X-CSRF-Token', token);
+        xhr.send(JSON.stringify(data));
+    };
+    var updateResults = function () {
+        var form = document.forms[0];
+        var data = {};
+        for (var i = 0, ii = form.length; i < ii; i++) {
+            var input = form[i];
+            if (input.type === 'radio' && input.checked) {
+                data[input.name] = input.value;
+            }
+            if (input.type === 'textarea') {
+                data[input.name] = input.value;
+            }
         }
-        if (input.type === 'textarea') {
-            data[input.name] = input.value;
+        var token = document.getElementById('token').value;
+        postJSON('/exam/update_results', token, data);
+    };
+    var start = function () {
+        var audio = document.getElementById('listening');
+        var exam = document.getElementById('exam-body');
+        var slide = document.querySelector('.slide');
+        exam.style.display = 'block';
+        slide.className = 'slide-up';
+        if (audio) {
+            audio.play();
         }
-    }
+        setInterval(function() {
+            updateResults();
+        }, 1000 * 60 * 5);
+    };
 
-    var token = document.getElementById('token').value;
-    postJSON('/exam/update_results', token, data);
-}
+    return {
+        start: start
+    };
+})();
--- website/static/js/user_script.js
+++ website/static/js/user_script.js
@@ -1,73 +1,71 @@
-function postJSON(url, token, data, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('X-CSRF-Token', token);
-    xhr.onload = callback;
-    xhr.send(JSON.stringify(data));
-}
-
-function empty(id) {
-    document.getElementById(id).innerHTML = '';
-}
-
-function html(id, text) {
-    document.getElementById(id).innerHTML = text;
-}
-
-function prepend(id, text) {
-    old = document.getElementById(id).innerHTML;
-    document.getElementById(id).innerHTML = text + old;
-}
-
-function showAddexaminee() {
-    prepend('addexaminee-names', this.responseText);
-}
-
-function addExaminee() {
-    var token = document.getElementById('examinee-token').value;
-    var data = {};
-
-    data['fullname'] = document.getElementById('addexaminee').value;
-    data['name'] = document.getElementById('studentcode').value;
-    data['getexam'] = document.getElementById('selectexam').value;
-    if (document.getElementById('addexaminee-names').innerHTML) {
-        data['button'] = true;
-    }
-    postJSON('/user/addexaminee', token, data, showAddexaminee);
-}
-
-function showGetscore() {
-    prepend('getscore-scores', this.responseText);
-}
-
-function getExamScore() {
-    var token = document.getElementById('score-token').value;
-    var data = {};
-
-    data['getscore'] = document.getElementById('getexamscore').value;
-    if (document.getElementById('getscore-scores').innerHTML) {
-        data['button'] = true;
-    }
-    postJSON('/user/examscore', token, data, showGetscore);
-}
-
-function showWrite() {
-    empty('checkwrite');
-    prepend('getexamscore', '<option>' + this.responseText + '</option>');
-}
-
-function sendWriteScore() {
-    var form = document.getElementById('writescore-form');
-    var token = document.getElementById('write-token').value;
-    var data = {};
-
-    for (var i = 0, ii = form.length; i < ii; i++) {
-        var input = form[i];
-        if (input.type === 'text') {
-            data[input.name] = input.value;
+var Admin = (function () {
+    var postJSON = function (url, token, data, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.setRequestHeader('X-CSRF-Token', token);
+        xhr.onload = callback;
+        xhr.send(JSON.stringify(data));
+    };
+    var empty = function (id) {
+        document.getElementById(id).innerHTML = '';
+    };
+    var prepend = function (id, text) {
+        old = document.getElementById(id).innerHTML;
+        document.getElementById(id).innerHTML = text + old;
+    };
+    var clearSignup = function () {
+        document.getElementById('signup-info').innerHTML = '<p>No students to contact.</p>'
+    };
+    var showAddExaminee = function () {
+        prepend('addexaminee-names', this.responseText);
+    };
+    var addExaminee = function () {
+        var token = document.getElementById('examinee-token').value;
+        var data = {};
+        data['fullname'] = document.getElementById('addexaminee').value;
+        data['name'] = document.getElementById('studentcode').value;
+        data['getexam'] = document.getElementById('selectexam').value;
+        if (document.getElementById('addexaminee-names').innerHTML) {
+            data['button'] = true;
         }
-    }
+        postJSON('/user/addexaminee', token, data, showAddExaminee);
+    };
+    var showGetScore = function () {
+        prepend('getscore-scores', this.responseText);
+    };
+    var getExamScore = function () {
+        var token = document.getElementById('score-token').value;
+        var data = {};
 
-    postJSON('/user/examwriting', token, data, showWrite);
-}
+        data['getscore'] = document.getElementById('getexamscore').value;
+        if (document.getElementById('getscore-scores').innerHTML) {
+            data['button'] = true;
+        }
+        postJSON('/user/examscore', token, data, showGetScore);
+    };
+    var showWrite = function () {
+        document.getElementById('checkwrite').innerHTML = '';
+        prepend('getexamscore', '<option>' + this.responseText + '</option>');
+    };
+    var sendWriteScore = function () {
+        var form = document.getElementById('writescore-form');
+        var token = document.getElementById('write-token').value;
+        var data = {};
+
+        for (var i = 0, ii = form.length; i < ii; i++) {
+            var input = form[i];
+            if (input.type === 'text') {
+                data[input.name] = input.value;
+            }
+        }
+        postJSON('/user/examwriting', token, data, showWrite);
+    };
+    return {
+        empty: empty,
+        clearSignup: clearSignup,
+        addExaminee: addExaminee,
+        getExamScore: getExamScore,
+        sendWriteScore: sendWriteScore
+    };
+})();
