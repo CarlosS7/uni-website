df337e8166d0e7797270fb704380bef1d6f24043
David Whitlock <alovedalongthe@gmail.com>
2015-03-18 16:12:35 +0700

    Added basic timer to exam script

--- website/static/js/exam_script.js
+++ website/static/js/exam_script.js
@@ -22,16 +22,24 @@ var Exam = (function () {
         postJSON('/exam/update_results', token, data);
     };
     var counter = function () {
+        var form = document.forms[0];
         var countdown = document.getElementById('countdown');
         var target_date = new Date().getTime() + 10800 * 1000;
         setInterval(function () {
             var current_date = new Date().getTime();
+            if (current_date > target_date) {
+                countdown.style.display = 'none';
+                form.submit();
+            }
             var seconds_left = (target_date - current_date) / 1000;
+            if (seconds_left < 900) {
+                countdown.className = 'deadline';
+            }
             hours = parseInt(seconds_left / 3600);
             seconds_left = seconds_left % 3600;
             minutes = ('0' + parseInt(seconds_left / 60)).slice(-2);
             countdown.innerHTML = 'Time remaining: ' + hours + ':' + minutes;
-        }, 1000 * 60);
+        }, 1000 * 30);
     };
     var start = function () {
         var audio = document.getElementById('listening');
