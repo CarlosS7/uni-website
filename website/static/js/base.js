a7df466d87a479a0f242431b2c2da8dd91036485
David Whitlock <alovedalongthe@gmail.com>
2015-08-13 18:42:01 +0700

    Reorganized javascript

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,12 +1,35 @@
-// functions needed site-wide
-
-function toggleDiv(showHideDiv) {
-    var el = document.getElementById(showHideDiv);
-    if (el.style.display == 'block') {
-        el.style.display = 'none';
-        el.style.visibility = 'hidden';
-    } else {
+var Base = (function () {
+    function showDiv(el) {
         el.style.display = 'block';
         el.style.visibility = 'visible';
     }
-} 
+
+    function hideDiv(el) {
+        el.style.display = 'none';
+        el.style.visibility = 'hidden';
+    }
+
+    function getStyle(el, name) {
+        var style;
+        if (typeof el.currentStyle != 'undefined'){
+            style = el.currentStyle;
+        }
+        else {
+            style = document.defaultView.getComputedStyle(el, null);
+        }
+        return  style[name];
+    }
+
+    function toggleDiv(divId) {
+        var el = document.getElementById(divId);
+        if (getStyle(el, 'display') == 'none') {
+            showDiv(el);
+        } else {
+            hideDiv(el);
+        }
+    }
+
+    return {
+        toggleDiv: toggleDiv
+    };
+}());
--- website/views/home.py
+++ website/views/home.py
@@ -1,14 +1,11 @@
 from flask import Blueprint, render_template
-from website.models import Content
 
 mod = Blueprint('home', __name__)
 
 @mod.route('/')
 def index():
-    data = Content.query.filter_by(content_id='home').first().content
-    return render_template('home/index.html', home_data=data)
+    return render_template('home/index.html')
 
 @mod.route('/about')
 def about():
-    data = Content.query.filter_by(content_id='about').first().content
-    return render_template('home/about.html', about_data=data)
+    return render_template('home/about.html')
