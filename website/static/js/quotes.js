ff919123896223d9640fde713840d02dd18eae3c
David Whitlock <alovedalongthe@gmail.com>
2015-08-31 15:24:04 +0700

    Added Thai course contact information

--- website/static/js/quotes.js
+++ website/static/js/quotes.js
@@ -3,10 +3,10 @@
 (function() {
     var quotes = [
     {q: 'I am very thankful to have taken this course -- it gave me a solid base from which to teach.', a: 'Alison, TESOL Certificate' },
-    {q: 'I learned a lot and now feel ready to study a university course in English.', a:'Seng, IEP Advanced'},
-    {q:'I study English in class and it is very interesting. Now I learn many things such as writing, speaking and listening skills. I really enjoy to meet friends and study here.', a:'Souaney, IEP Intermediate'},
-        {q:'This class helped my English get better and I meet new people. I really like this class because it is fun and makes me improve my English.', a:'Sal Lao Khay Sue, IEP Intermediate'},
-        {q:'My TESOL training has given me invaluable resources for working with my students. I\'m continuously pulling from things I learned in my training to find the most effective teaching technique with each particular student.', a:'Katie, TESOL Certificate'}
+    {q: 'I learned so many things and now have the confidence to study a university course in English.', a:'Seng, IEP Advanced'},
+    {q: 'I study English in class and it is very interesting. I really like studying here with my new friends.', a:'Souaney, IEP Intermediate'},
+    {q: 'This class helped my English get better. I really like it. It is fun and helps me improve my English.', a:'Sal Lao Khay Sue, IEP Intermediate'},
+    {q: 'My TESOL training has given me invaluable resources for working with my students.', a:'Katie, TESOL Certificate'}
     ];
     var randQuotes = function (quotes) {
         var i = Math.floor(Math.random()*quotes.length);
